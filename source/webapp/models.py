f0410bce5c19979a58e8a1aad94c40817609b7f9
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2023-05-26 23:01:14 +0600

    added model Comment

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class Comment(models.Model):
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=50, default='Unknown', verbose_name='Автор')
+    article = models.ForeignKey('webapp.Article', on_delete=models.CASCADE, related_name='comments', verbose_name='Статья')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return f'{self.pk}. {self.text[:20]}'
+
+
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name='Заголовок')
     author = models.CharField(max_length=50, verbose_name='Автор', default='Unknown')
